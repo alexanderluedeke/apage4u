7c01a21f05dbc057c8b41c3fd862b8abd7dfac07
apage4u <meine.services@googlemail.com>
2011-04-13 22:35:34 +0200

    Disclaimer link in footer and disclaimer page added.

---
+++ disclaimer.php
@@ -0,0 +1,77 @@
+<?php session_start(); $nav='homepage'; ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="description" content="apage4u. Die Internet-Plattform von Alexander Lüdeke." />
+		<meta name="keywords" content="Alexander, Alex, Lüdeke, Lüdecke, Lüdekke, Lüddeke, music, web, design, programming, osnabrück, niedersachsen, berlin" />
+		<title>apage4u - disclaimer</title>
+		<link rel="stylesheet" title="Standard" type="text/css" href="css/layout.css" media="all" />
+		<link  href="//fonts.googleapis.com/css?family=Arvo:regular,italic,bold,bolditalic" rel="stylesheet" type="text/css" />
+		<!--[if IE]>
+			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+	</head>
+	<body id="apage4u" class="<?php $nav ?>">
+		<div class="page">
+			<header>
+			    <h1><a href="/" title="Homepage"><span class="a">A</span><span class="page">Page</span><span class="you">4You</span></a></h1>
+				<?php include($_SERVER['DOCUMENT_ROOT'] . '/incl/nav_main.php'); ?>
+			</header>
+			<article>
+			
+				<header>
+					<h2>Disclaimer</h2>		        
+				</header>
+				
+				<p>Impressum gemäß § 5 TMG:</p>
+                
+                <p>Alexander Lüdeke<br />
+                10245 Berlin, Friedrichshain<br />
+                Germany</p>
+
+                <h3>Kontakt</h3>
+
+                <p>E-Mail: impressum.hearandknow()apage4u.de</p>
+
+                <h3>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV:</h3>
+
+                <p>Alexander Lüdeke<br />
+                10245 Berlin, Friedrichshain<br />
+                Germany</p>
+
+                <h3>Haftungsausschluss</h3>
+
+                <h4>Haftung für Inhalte</h4>
+                
+                <p>Die Inhalte dieses Website wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte kann ich jedoch keine Gewähr übernehmen.</p>
+
+                <h4>Haftung für Links</h4>
+
+                <p>Diese Website enthält Links zu externen Webseiten Dritter, auf deren Inhalte ich keinen Einfluss habe.   Deshalb kann ich für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werde ich derartige Links umgehend entfernen.</p>
+
+                <h4>Urheberrecht</h4>
+
+                <p>Die durch mich erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitte ich um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werde ich derartige Inhalte umgehend entfernen.</p>
+
+                <p>Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit ausdrücklich widersprochen. Der Betreiber dieser Website behält sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor.</p>
+			</article>
+			<?php include($_SERVER['DOCUMENT_ROOT'] . '/incl/footer.php'); ?>
+		</div>
+		<script type="text/javascript">
+		
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-11922467-2']);
+		  _gaq.push(['_trackPageview']);
+		
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		
+		</script>
+	</body>
+</html>
+
+
--- incl/footer.php
+++ incl/footer.php
@@ -1,6 +1,8 @@
 			<footer>
 				<aside> <div id="hak-results"></div> <div id="twitter-results"></div> <div id="del-results"></div></aside>
-				© Copyright 2006-<?php echo date('Y'); ?>
+				© Copyright 2006-<?php echo date('Y'); ?> 
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				<a href="/disclaimer.php">Disclaimer</a>
 				<script type="text/javascript" src="http://query.yahooapis.com/v1/public/yql?q=select%20title%2C%20link%2C%20description%20from%20rss%20where%20url%3D%22http%3A%2F%2Fhearandknow.wordpress.com%2Ffeed%2F%22%20limit%203&amp;format=json&amp;diagnostics=true&amp;callback=hak"></script>
 				<script type="text/javascript" src="http://query.yahooapis.com/v1/public/yql?q=select%20title%2C%20link%20from%20rss%20where%20url%3D%22https%3A%2F%2Ftwitter.com%2Fstatuses%2Fuser_timeline%2F140541482.rss%22%20limit%207&amp;format=json&amp;diagnostics=true&amp;callback=twitter"></script>
 				<script type="text/javascript" src="http://query.yahooapis.com/v1/public/yql?q=select%20title%2C%20link%2C%20description%20from%20rss%20where%20url%3D%22http%3A%2F%2Ffeeds.feedburner.com%2Fapage4u%2Ffavorites%22%20limit%203&amp;format=json&amp;diagnostics=true&amp;callback=del"></script>
