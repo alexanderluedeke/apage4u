602e8146264f7b5e2e6bbd4fc4253c114ff867e4
Alexander Lüdeke <alexanderluedeke@users.noreply.github.com>
2016-07-28 15:24:47 +0200

    Small changes.

--- about/index.php
+++ about/index.php
@@ -25,13 +25,18 @@
 					<h2>A little bit about what I do</h2>		        
 				</header>
 				
-				<p>Hi, I'm Alexander Lüdeke from Berlin and I develop web and audio applications. My current job title is <a href="../programming/">java software developer</a>, which basically means I build the part of a website that connects the back and front end. I'm comfortable with working on projects from start to finish, all aspects of development in a complex environment.</p>
+				<p>Hi, I'm Alexander Lüdeke from berlin, germany. My current job title is <a href="../programming/">senior java software developer</a>. I develop web and audio applications. That means basically I build all parts of a web application on my own just with the help of common frameworks and - of course - my colleagues. I'm comfortable with working on projects from start to finish, all aspects of development in a complex environment.</p>
 				
 				<p>In my hometown Osnabrück I studied <a href="../science/systemScience.php">applied system science</a>, so I am experienced in analysis and modelling dynamic systems.</p>
 				
 				<p>But that is not all, I am concerned with <a href="../music/">digital audio production</a> – especially working with ableton live.</p>
 				
-				<p>In case that is not enough for you, please check out my <a href="http://www.linkedin.com/in/alexanderluedeke" target="_linkedin">LinkedIn profil</a> or just <a href="../contact.php">send me a message</a>.</p>
+				<p>In case that is not enough for you, please check out my <a href="http://www.linkedin.com/in/alexanderluedeke">LinkedIn profil</a> or just <a href="../contact.php">send me a message</a>.</p>
+			
+        <br />
+        <br />
+		
+				<p>Alexander Lüdeke (System scientist and senior software developer)</p>
 				
 			</article>
 			<?php include($_SERVER['DOCUMENT_ROOT'] . '/incl/footer.php'); ?>
